2ea0951f866c0f98d96c4efabc0360a4a9900a5e
sahayquant196 <nishantiitbbs@gmail.com>
2024-05-18 03:49:42 +0100

    Util Functions

---
+++ utils.py
@@ -0,0 +1,4 @@
+def user_inputs():
+    player1_name = input('Please enter your name')
+    userinput = input('Type C for playing against computer,press any other key for manual player')
+    return player1_name,userinput
